ec14a9b15270acfdfb847cbb2b00735b927e1613
Georgios Kaleadis <georgios@kaleadis.de>
2019-01-10 23:19:43 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,17 @@ if(Reveal) {
   Reveal.addEventListener( 'slidechanged', function( {currentSlide} ) {
     const state = currentSlide.dataset.state;
 
+
+    const codepenToLoad = currentSlide.querySelector('.codepen-lazy-load');
+    if(codepenToLoad) {
+      console.log('Starting Pen on current slide');
+      // remove our marker, replace with class codepen so that the native embed function from codepen (__CPEmbed)
+      // will find it and initialize it.
+      codepenToLoad.classList.remove('codepen-lazy-load');
+      codepenToLoad.classList.add('codepen');
+      __CPEmbed();
+    }
+    console.log()
     // switch(state) {
     //   case 'fact-code-01':
     //     runExample01(currentSlide);
